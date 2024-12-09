cce3f28dfcee5d7fedb614938d1de718713a2502
山雨欲来风满楼 <maqingshui@outlook.com>
2024-12-09 11:48:30 +0800

    完善了采样率部分，修复精度丢失问题。Scottie-DX目前可用。
    Co-Authored-By: BI4PYM <24314717+bi4pym@users.noreply.github.com>

--- sstv.c
+++ sstv.c
@@ -1,19 +1,41 @@
+/* 
+
+This C program is used to modulate SSTV audio.
+It will be used for amateur radio activities with the XuanJing series satellites.
+
+Version: 1.0   Date: December 9, 2024
+
+Developer & Acknowledgments:
+    BG7ZDQ - Initial program
+    BI4PYM - Enhancements and fixes
+    N7CXI  - Document: "Proposal for SSTV Mode Specifications"
+
+License: GNU General Public License v3.0
+
+*/
+
+#define STB_IMAGE_IMPLEMENTATION          // stb预处理器
+#define STB_IMAGE_RESIZE_IMPLEMENTATION   // stb预处理器 
+#define SAMPLE_RATE 11025                 // 采样率
+#define PI 3.14159265358979323846         // 圆周率
+#define COLOR_FREQ_MULT 3.1372549         // 颜色频率乘数，用于转换RGB值到频率(0-255映射到1500-2300)
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <stb_image.h>
-#include <stb_image_write.h>
 #include <windows.h>
-
-// 定义一些常量，用于调制产生音频和控制音频文件的参数
-#define SAMPLE_RATE 44100
-#define PI 3.14159265358979323846
+#include "include\stb_image.h"
 
 // 声明全局变量
 FILE *file;                   // 用于存储音频数据的文件指针
-uint32_t total_samples;   // 总采样数
+uint32_t total_samples;       // 总采样数
+const unsigned char *pixels;  // 图像像素数据
+int width;                    // 图像宽度
+double olderdata;             // 前一个幅度，用于连续相位
+double oldercos;              // 前一个COS，用于连续相位
+double delta_lenth = 0;       // 采样率精度补偿
 
 // WAV 文件头结构体，用于存储 WAV 文件格式的头部信息
 typedef struct {
@@ -35,64 +57,70 @@ typedef struct {
 // 生成 WAV 文件头
 void write_wav_header(uint32_t data_size) {
     WAVHeader header = {
-        .riff = "RIFF",                        // "RIFF" 文件标识
-        .chunk_size = 36 + data_size,          // 文件头与数据部分的总大小
-        .wave = "WAVE",                        // "WAVE" 文件标识
-        .fmt = "fmt ",                         // "fmt " 格式标识
-        .subchunk1_size = 16,                  // 格式块大小为 16 字节
-        .audio_format = 1,                     // PCM 格式
-        .num_channels = 1,                     // 单声道
-        .sample_rate = SAMPLE_RATE,            // 设置采样率
-        .byte_rate = SAMPLE_RATE * 1 * 16 / 8, // 每秒字节数
-        .block_align = 1 * 16 / 8,             // 块对齐
-        .bits_per_sample = 16,                 // 每个样本 16 位
-        .data = "data",                        // 音频数据标识
-        .subchunk2_size = data_size            // 音频数据大小
+        .riff = "RIFF",
+        .chunk_size = 36 + data_size,
+        .wave = "WAVE",
+        .fmt = "fmt ",
+        .subchunk1_size = 16,
+        .audio_format = 1,
+        .num_channels = 1,
+        .sample_rate = SAMPLE_RATE,
+        .byte_rate = SAMPLE_RATE * 1 * 16 / 8,
+        .block_align = 1 * 16 / 8,
+        .bits_per_sample = 16,
+        .data = "data",
+        .subchunk2_size = data_size
     };
-    fseek(file, 0, SEEK_SET);                  // 确保写在文件开头
-    fwrite(&header, sizeof(WAVHeader), 1, file);  // 将文件头写入文件
+    fseek(file, 0, SEEK_SET);
+    fwrite(&header, sizeof(WAVHeader), 1, file);
 }
 
-// 函数：生成并写入指定频率和持续时间的正弦波音频
-void write_tone(double frequency, double duration_ms) {
-    uint32_t num_samples = (SAMPLE_RATE * duration_ms) / 1000;
+// 函数：生成并写入指定频率和持续时间和初始相位的正弦波音频
+void write_tone(double frequency, double duration_ms, double phi) {
+    uint32_t num_samples = SAMPLE_RATE * duration_ms / 1000;
+    delta_lenth += SAMPLE_RATE * duration_ms / 1000 - num_samples;
+    if (delta_lenth >= 1) {
+        num_samples += (int)delta_lenth;
+        delta_lenth -= (int)delta_lenth;
+    }
+    double phi_samples = SAMPLE_RATE * phi;
     short buffer[num_samples];
     for (uint32_t i = 0; i < num_samples; ++i) {
-        buffer[i] = (short)(32767 * sin(2 * PI * frequency * i / SAMPLE_RATE));
+        buffer[i] = (short)(32767 * sin((2 * PI * frequency * i + phi_samples) / SAMPLE_RATE));
     }
     fwrite(buffer, sizeof(short), num_samples, file);
-
-    // 计算并累加总样本数
     total_samples += num_samples;
+	olderdata = sin((2 * PI * frequency * num_samples + phi_samples) / SAMPLE_RATE);
+    oldercos = cos((2 * PI * frequency * num_samples + phi_samples) / SAMPLE_RATE);
+    //printf("%lf\n",phi_samples);
+    //printf("%lf\n",olderdata);
 }
+// 函数：判断正负号
+int sign(double num) {
 
-void sstvmodel(const char *model){
-    if(model == "PD120"){
-
+    if (num >= 0) {
+        return 1;
+    } else if (num < 0) {
+        return -1;
+    } else {
+        return 0;
     }
 }
-
-// 开始调制
-void generate(const char *filename, const char *vis_code, const char *model) {
-    total_samples = 0;    // 重置样本计数
-    file = fopen(filename, "wb");
-    if (!file) { perror("无法打开文件"); return; }
-
-    write_wav_header(0);    // 写入空的 WAV 头
-
-    // 快速识别前导+VIS码引导音与起始音部分
+// 函数：调制 VIS 校准头
+void generate_vis(const char *vis_code) {
+    
+    // 快速识别前导 + VIS 码引导音与起始音部分
     struct { double frequency; int duration_ms; } tones[] = {
         {1900, 100},{1500, 100},{1900, 100},{1500, 100},{2300, 100},{1500, 100},{2300, 100},{1500, 100},{1900, 300}, {1200, 10}, {1900, 300}, {1200, 30}
     };
     for (int i = 0; i < 12; i++) {
-        write_tone(tones[i].frequency, tones[i].duration_ms);
+        write_tone(tones[i].frequency, tones[i].duration_ms, 0);
     }
 
-    // VIS 码 7 位标识数据位部分
+    // VIS 码 7 位标识数据位部分，注意 VIS 码为小端序
     for (int i = 0; i < 7; i++) {
-        // 注意 VIS 码为小端序，所以从右到左读取(即 6-i )
         double frequency = vis_code[6-i] == '1' ? 1100 : 1300;
-        write_tone(frequency, 30);
+        write_tone(frequency, 30, 0);
     }
 
     // 偶校验位部分
@@ -103,43 +131,121 @@ void generate(const char *filename, const char *vis_code, const char *model) {
         }
     }
     int parity_bit = (parity % 2 == 0) ? 0 : 1;
-    write_tone((parity_bit == 0) ? 1300 : 1100, 30);
+    write_tone((parity_bit == 0) ? 1300 : 1100, 30, 0);
 
     // 结束位
-    write_tone(1200, 30);
+    write_tone(1200, 30, 0);
+}
 
+// 函数：计算像素在某一颜色通道的强度
+double rgb_read_pixel(const char *color, int x, int y) {
+    int index = (y * width + x) * 3;
+
+    if (strcmp(color, "r") == 0) {
+        return (double)pixels[index];
+    } else if (strcmp(color, "g") == 0) {
+        return (double)pixels[index + 1];
+    } else if (strcmp(color, "b") == 0) {
+        return (double)pixels[index + 2];
+    } else {
+        fprintf(stderr, "无效的颜色参数: %s\n", color);
+        return -1.0; // 错误返回值
+    }
+}
+
+// 函数：调制 Scottie-DX 图像
+void generate_scottie_dx() {
     
+    // 起始同步脉冲，仅第一行
+    write_tone(1200, 9, 0);
+
+    // 图像数据部分
+    for(int line = 0; line < 256; line++) {
+        // 分离脉冲
+        write_tone(1500, 1.5, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+
+        // 绿色扫描
+        for(int x = 0; x < 320; x++) {
+            write_tone(1500 + rgb_read_pixel("g",x,line)*COLOR_FREQ_MULT, 1.08, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+            //printf("Green freq: %lf\n", 1500 + rgb_read_pixel("g",x,line)*COLOR_FREQ_MULT);
+        }
+
+        // 分离脉冲
+        write_tone(1500, 1.5, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+
+        // 蓝色扫描
+        for(int x = 0; x < 320; x++) {
+            write_tone(1500 + rgb_read_pixel("b",x,line)*COLOR_FREQ_MULT, 1.08, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+            //printf("Blue freq: %lf\n", 1500 + rgb_read_pixel("b",x,line)*COLOR_FREQ_MULT);
+        }
+
+        // 同步脉冲与同步沿
+        write_tone(1200, 9, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+        write_tone(1500, 1.5, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+
+        // 红色扫描
+        for(int x = 0; x < 320; x++) {
+            write_tone(1500 + rgb_read_pixel("r",x,line)*COLOR_FREQ_MULT, 1.08, sign(oldercos) * asin(olderdata) + abs(sign(oldercos) - 1) / 2 * PI);
+            //printf("Red freq: %lf\n", 1500 + rgb_read_pixel("r",x,line)*COLOR_FREQ_MULT);
+        }
+    }
+}
+
+// 主调制生成函数
+int generate(const char *wav_filename, const char *model) {
+    
+    // 文件写入初始化
+    total_samples = 0;    // 重置样本计数
+    file = fopen(wav_filename, "wb");
+    if (!file) { perror("无法打开文件"); return 1; }
+    write_wav_header(0);    // 写入空的 WAV 头
+
+    // 按模式选择 VIS 码，调制图像内容
+    if (strcmp(model, "Scottie-DX") == 0){
+        generate_vis("1001100");
+        generate_scottie_dx();
+    }
 
     // 计算数据大小并写入文件头
     uint32_t data_size = total_samples * sizeof(short);
     write_wav_header(data_size);
     fclose(file);
-
-    printf("WAV 文件已生成: %s\n", filename);
+    return 0;
 }
 
-int main() {
-    char filename, vis_code, model;
+int main(int argc, char *argv[]) {
+    int height, channels;
+    const char *image_filename;
+    const char *model;
+    image_filename = argv[1];
+    model = argv[2];
     SetConsoleOutputCP(65001);
 
-    scanf("%c %c", filename, model);
-
-    if(model == "PD120"){
-        vis_code = "1011111";
-    }else if(model == "Scottie 1"){
-        vis_code = "0111100";
-    }else if(model == "Scottie 2"){
-        vis_code = "0111000";
-    }else if(model == "Scottie DX"){
-        vis_code = "1001100";
-    }else{
-        printf("不支持的SSTV模式");
-        return 0;
+    // 命令行提示
+    if (argc != 3) {
+        printf("用法: .\\sstv.exe <image_filename> <SSTV model>\n");
+        printf("支持的SSTV模式: Scottie-DX\n");
+        return 1;
     }
 
-    generate(filename, vis_code, model);
+    // 读取图像
+    pixels = stbi_load(image_filename, &width, &height, &channels, 3);
+    if (!pixels) {
+        fprintf(stderr, "无法加载图像文件。\n");
+        return 1;
+    }
 
-    return 0;
+    // 处理 WAV 文件名
+    char wav_filename[256];
+    snprintf(wav_filename, sizeof(wav_filename), "%s.wav", strtok(strdup(image_filename), "."));
+
+    // 开始调制
+    int result = generate(wav_filename, model);
+
+    // 释放图像内存
+    stbi_image_free((unsigned char*)pixels);
+    printf("End.");
+    return result;
 }
 
 /*
